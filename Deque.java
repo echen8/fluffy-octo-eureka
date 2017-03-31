a60cd6de3a97b5cde42743fbadefd6a440c313d5
aroktim@stuy.edu <aroktim@stuy.edu>
2017-03-31 10:56:31 -0400

    Added Deque interface

---
+++ Deque.java
@@ -0,0 +1,11 @@
+public interface Deque<T>{
+
+    public void addFirst( T t );
+    public void addLast( T t );
+    public T getFirst();
+    public T getLast();
+    public T removeFirst();
+    public T removeLast();
+    public int size();
+
+}
