1896e9ccb36d71eaaf3a8d23a950df29adbe34dc
ArifRoktim <aroktim@stuy.edu>
2017-04-01 21:00:42 -0400

    Added methods for FIFO/FILO behavior

--- Deque.java
+++ Deque.java
@@ -8,4 +8,14 @@ public interface Deque<T>{
     //public T removeLast();
     //public int size();
 
+    // Methods for FIFO behavior
+    //public void add( T t );
+    //public T remove( T t );
+
+    // Methods for FILO behavior
+    //public void push( T t );
+    //public T pop();
+
+    // For both FILO and FIFO
+    //public T peek();
 }
